48e4200f983a54d2f8f1e0292552812604e4b7b8
suirik <suiriksamatkyzy@gmail.com>
2026-02-05 22:55:51 +0500

    Add boolean_intro.py to demonstrate boolean values

---
+++ practice2/boolean/boolean_intro.py
@@ -0,0 +1,15 @@
+# Boolean values
+print(True)
+print(False)
+
+# Booleans from comparisons
+print(3 > 1)
+print(5 == 2)
+print(4 != 4)
+
+# Boolean variables
+x = 6
+y = 9
+print(x < y)
+print(x >= 10)
+ 
