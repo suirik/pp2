10ec97878a4122283a063cf6f7d2a46c02c2aeee
suirik <suiriksamatkyzy@gmail.com>
2026-02-05 22:56:44 +0500

    Add if-else examples for number and temperature checks
    Added examples of if-else statements for number positivity, even/odd checks, and temperature classification.

---
+++ practice2/if_else/if_else.py
@@ -0,0 +1,19 @@
+# if – else examples
+
+num = int(input("Number: "))
+if num > 0:
+    print("Positive")
+else:
+    print("Negative or zero")
+
+a = int(input("A: "))
+if a % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+temp = int(input("Temperature: "))
+if temp >= 20:
+    print("Warm") 
+else:
+    print("Cold")
