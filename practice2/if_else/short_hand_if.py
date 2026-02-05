1a417e54223b42355ef05009ad68907686244e70
suirik <suiriksamatkyzy@gmail.com>
2026-02-05 22:57:10 +0500

    Implement short-hand if examples in short_hand_if.py
    Added examples of short-hand if statements in Python.

---
+++ practice2/if_else/short_hand_if.py
@@ -0,0 +1,12 @@
+# Short-hand if examples
+
+a = 4
+b = 9
+print("a is bigger") if a > b else print("b is bigger")
+
+x = int(input("X: "))
+print("Positive") if x > 0 else print("Zero or negative")
+
+age = int(input("Age: "))
+print("Adult") if age >= 18 else print("Minor")
+ 
