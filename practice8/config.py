85783c90aadd145aa6c07ea9a8798a91b9445a05
suirik <suiriksamatkyzy@gmail.com>
2026-04-17 00:13:40 +0500

    Add PostgreSQL configuration details

---
+++ practice8/config.py
@@ -0,0 +1,8 @@
+# Configuration details for PostgreSQL connection
+params = {
+    "host": "localhost",
+    "database": "phonebook_db",
+    "user": "postgres",
+    "password": "Suismtk1029",
+    "port": "5432"
+}
